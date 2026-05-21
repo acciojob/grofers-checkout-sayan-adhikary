281746aee7c832d28b1e13b782f10a4ac023c887
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-05-22 01:45:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
-let end = 0;
-const getSum = () => {
-//Add your code here
-	if (end == 1) {
-        return;
-    }
-    end = 1;
-    let sum = 0;
-    let price = document.querySelectorAll(".prices");
+let prices = document.querySelectorAll(".prices");
 
-    for (let i = 0; i < price.length; i++) {
-        let temp = price[i].textContent;
-        sum += Number(temp);
-    }
+let total = 0;
 
-    let tr = document.createElement('tr');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    td1.textContent = "Total Cost : ";
-    td2.textContent = `${sum}`;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
+prices.forEach((price) => {
+    total += Number(price.textContent);
+});
 
-    // body.insertBefore(tr, btn);
-    let table = document.querySelector('table');
-    table.appendChild(tr);
-  
-};
+let table = document.querySelector("table");
 
-getSumBtn.addEventListener("click", getSum);
+let newRow = document.createElement("tr");
 
+let totalCell = document.createElement("td");
+
+totalCell.colSpan = 2;
+totalCell.textContent = "Total Price: " + total;
+
+newRow.appendChild(totalCell);
+
+table.appendChild(newRow);
