6e6f55b93e59e3c682da8a0c15698555d90a75db
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-05-23 12:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-let prices = document.querySelectorAll(".prices");
+let btn = document.createElement("button");
+btn.textContent = "Total Price";
 
-let total = 0;
+let body = document.querySelector("body");
+body.appendChild(btn);
+let end = 0;
+btn.addEventListener('click', () => {
+    if (end == 1) {
+        return;
+    }
+    end = 1;
+    let sum = 0;
+    let price = document.querySelectorAll(".prices");
 
-prices.forEach((price) => {
-    total += Number(price.textContent);
-});
+    for (let i = 0; i < price.length; i++) {
+        let temp = price[i].textContent;
+        sum += Number(temp);
+    }
 
-let table = document.querySelector("table");
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    td1.textContent = "Total Cost : ";
+    td2.textContent = `${sum}`;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
 
-let newRow = document.createElement("tr");
-
-let totalCell = document.createElement("td");
-
-totalCell.colSpan = 2;
-totalCell.textContent = "Total Price: " + total;
-
-newRow.appendChild(totalCell);
-
-table.appendChild(newRow);
+    // body.insertBefore(tr, btn);
+    let table = document.querySelector('table');
+    table.appendChild(tr);
+})
